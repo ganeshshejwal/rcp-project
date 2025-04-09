988b0aaef2cda3cc89399688c02c0830dc494764
ganeshshejwal <ganesh201@gmail.com>
2025-04-09 14:38:34 +0530

    Add config-loader

---
+++ config-loader/src/main/java/com/cennox/config_loader/ConfigLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.cennox.config_loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigLoaderApplication.class, args);
+	}
+
+}
---
+++ config-loader/src/test/java/com/cennox/config_loader/ConfigLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cennox.config_loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
